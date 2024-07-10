2eedb7e91a2b4fb9635c1cda84655273fb8b0f62
parsakhosravani <parsakhosravani@gmail.com>
2024-07-10 20:15:42 +0330

    🚀 publish tribe game

---
+++ config/site.ts
@@ -0,0 +1,62 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Tribes",
+  description: "Make beautiful websites regardless of your design experience.",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Resources",
+      href: "/docs",
+    },
+    {
+      label: "Army",
+      href: "/pricing",
+    },
+    {
+      label: "Allies",
+      href: "/blog",
+    },
+    {
+      label: "World",
+      href: "/about",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Mine More",
+      href: "/profile",
+    },
+    {
+      label: "Random Boost",
+      href: "/dashboard",
+    },
+    {
+      label: "Full Charge",
+      href: "/projects",
+    },
+    {
+      label: "Increase Capacity",
+      href: "/team",
+    },
+    {
+      label: "Calendar",
+      href: "/calendar",
+    },
+    {
+      label: "Settings",
+      href: "/settings",
+    },
+    {
+      label: "Help & Feedback",
+      href: "/help-feedback",
+    },
+    {
+      label: "Logout",
+      href: "/logout",
+    },
+  ],
+};
