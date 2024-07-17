fd5a45ca62d9194436033e6a98f633168684b657
Parsa <parsakhosravani@gmail.com>
2024-07-17 22:54:10 +0330

    🚨 fix eslint

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,7 @@ import { Card } from "@nextui-org/card";
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <Card isBlurred>Salam</Card>
-      <Card isBlurred>Salam</Card>
-      <Card isBlurred>Salam</Card>
+      <Card isBlurred>Salam Resources !!!</Card>
     </section>
   );
 }
---
+++ components/templates/airdrop.tsx
@@ -0,0 +1,3 @@
+export const airdrop = () => {
+  return null;
+};
