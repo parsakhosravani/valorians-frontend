58eac787d3630cc078e4b9521fd7fb4d2ed6c84e
parsakhosravani <parsakhosravani@gmail.com>
2024-07-21 21:13:08 +0330

    🍱 update intro bg

--- components/templates/intro.tsx
+++ components/templates/intro.tsx
@@ -10,7 +10,7 @@ export const Intro = () => {
     <div className="absolute bottom-0 flex h-dvh-[90%] w-full items-center justify-center rounded-t-[50px] bg-[#0D0F12] px-4 pt-6">
       <img
         alt="background image"
-        src={background.src}
+        src="https://s3-alpha-sig.figma.com/img/d905/0762/c4c97cb500dabbfec2ab8f24c7c81a65?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=XlR-BU1~tty5-stTlo7HPwVaw5eUPh4j0vYoQECNWE~W1GcCFE5UauReFcqkSCSM6RJQ9vPOe7HoAqP2CW6wOJ0R7520zZfnfeQnhoWIcNkUB11aLrlCjZkLXoC32-9w1KJII8RhXe4pU5jAJKeqiqCup0B6EnwTBqQnr0X7Kv5KONctoR1s4TmAUb7Tzkx-TY7YYlckgb8NRwlK3~mcJOte3AqvkmyhSRiC1tItA8uTI6vUBrKSffJOrW9AFh9gju8X9kNOwUa6Yp~yCMEhi1MMjn54NjTOMyQmv3YNjg4NXUfZjW9Lg2KUyHnslUz-~KCFnMJV19gCg1J6r3oh9w__"
         className="rounded-t-[50px]"
       />
 
