5e57a0069feeb338217c05068bf0d1e1e866934b
h.khosravani <parsakhosravani@gmail.com>
2024-07-22 14:49:46 +0330

    💄 fix tap

--- components/organisms/activeResource.tsx
+++ components/organisms/activeResource.tsx
@@ -90,7 +90,7 @@ export const ActiveResource = ({
             +{earnLevelEnergy}
           </span>
         ))}
-        <div className="w-full h-full group-active:animate-[scale_.2s_cubic-bezier(0.18,0.89,0.32,1.28)]">
+        <div className="w-full h-full group-active:animate-[scale_0.3s_ease-in-out]">
           <img src={activeResource?.img} alt={activeResource.name} />
         </div>
       </div>
