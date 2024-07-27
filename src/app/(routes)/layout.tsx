523e872d1568d518a3b1cfa1a8f4a17b6e3491c7
Parsa <parsakhosravani@gmail.com>
2024-07-28 02:41:25 +0330

    🍱 add roboto font

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "@/styles/globals.css";
 import { Navbar } from "@/components";
 import { Providers } from "../providers";
+import { Roboto } from "next/font/google";
 
 import GoogleScript from "@/scripts/GoogleScript";
 import TelegramScript from "@/scripts/TelegramScript";
@@ -11,13 +12,20 @@ export const metadata: Metadata = {
   description: "Valorians legend is a mini game",
 };
 
+const roboto = Roboto({
+  weight: ["400", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html suppressHydrationWarning lang="en">
+    <html suppressHydrationWarning lang="en" className={roboto.className}>
       <body className="bg-black text-white">
         <Providers>
           <div className="relative flex flex-col overflow-hidden h-screen">
