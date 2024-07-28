315d2459f054a54b47c0e7c2e2b3f3c67ca6b78e
Parsa <parsakhosravani@gmail.com>
2024-07-28 03:58:40 +0330

    💄 add bg quest

--- src/components/templates/Quest.tsx
+++ src/components/templates/Quest.tsx
@@ -4,6 +4,6 @@ interface QuestPropsType {}
 
 export const Quest: FunctionComponent<QuestPropsType> = () => {
   return (
-    <div className="bg- bg-no-repeat bg-cover bg-center w-full h-full"></div>
+    <div className="bg-quest bg-no-repeat bg-cover bg-center w-full h-full"></div>
   );
 };
