327f3ec88ff066cc45b9a6ad2bbe0bec6da645d9
mehdiskan <mehdiskan@gmail.com>
2024-07-30 05:22:17 +0330

    fix: tribe page background

--- src/components/templates/Tribe.tsx
+++ src/components/templates/Tribe.tsx
@@ -3,6 +3,8 @@ import { Building } from "../organisms";
 import mainBuilding from "~/images/building/mainBuilding.webp";
 import barracksBuilding from "~/images/building/barracksBuilding.webp";
 import resourcesBuilding from "~/images/building/resourcesBuilding.webp";
+import bg from "~/images/background/tribe.webp";
+import Image from "next/image";
 
 interface TribePropsType { }
 
@@ -14,6 +16,7 @@ export const Tribe: FunctionComponent<TribePropsType> = () => {
   };
 
   return (
+    <>
     <div className="m-2 my-0 h-auto">
       <div className="grid grid-cols-2 gap-2.5 w-full overflow-y-auto scrollable max-h-[85svh] pb-10">
         {[...Array(20)].map((_, index) => (
@@ -23,5 +26,13 @@ export const Tribe: FunctionComponent<TribePropsType> = () => {
         ))}
       </div>
     </div>
+
+    <Image
+        priority
+        src={bg}
+        alt={'intro'}
+        className="absolute h-full top-0 left-0 w-full -z-10 object-cover"
+      />
+    </>
   );
 };
